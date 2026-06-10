bcf203ddd0e71820c8769e6a0c197291fb336f69
Dodoroshka <yanovaanastasia18@gmail.com>
2026-06-10 15:59:33 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,26 +2,25 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 #include <vector>
-class Tree {
-public:
-    struct Node {
-        char value;
-        std::vector<Node*> branches;
-        Node(char v = 0) : value(v) {}
-    };
-private:
-    Node* root;
-    std::vector<char> source;
-    Node* construct(const std::vector<char>& remaining);
-    void erase(Node* vertex);
-public:
-    explicit Tree(const std::vector<char>& input);
-    ~Tree();
-    Node* getRoot() const { return root; }
-    const std::vector<char>& getSource() const { return source; }
+class PMTree {
+ public:
+  struct Node {
+    char value;
+    std::vector<Node*> branches;
+    explicit Node(char v = 0) : value(v) {}
+  };
+ private:
+  Node* root;
+  std::vector<char> source;
+  Node* construct(const std::vector<char>& remaining);
+  void erase(Node* vertex);
+ public:
+  explicit PMTree(const std::vector<char>& input);
+  ~PMTree();
+  Node* getRoot() const { return root; }
+  const std::vector<char>& getSource() const { return source; }
 };
-std::vector<std::vector<char>> getAllPerms(Tree& container);
-std::vector<char> getPerm1(Tree& container, int position);
-std::vector<char> getPerm2(Tree& container, int position);
-
+std::vector<std::vector<char>> getAllPerms(PMTree& container);
+std::vector<char> getPerm1(PMTree& container, int position);
+std::vector<char> getPerm2(PMTree& container, int position);
 #endif  // INCLUDE_TREE_H_
